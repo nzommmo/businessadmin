91352d29e6fc7680add05c8fbd642a1aa738fb56
nzommmo <ericnzomo17@gmail.com>
2024-07-21 16:24:39 +0300

    SKELETON

---
+++ FRONTEND/Businessadmin/src/App.jsx
@@ -0,0 +1,14 @@
+import NavBar from "./components/NavBar"
+import HeroSection from "./components/HeroSection"
+
+const App = () => {
+  return (
+    <>
+    <NavBar/>
+    <HeroSection/>
+    
+    </>
+  )
+}
+
+export default App
