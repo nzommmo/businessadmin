6165b6c668eaecf2b81311bfc2fdf5070d715f54
lewis-andy <lewisandanje3@gmail.com>
2024-07-21 19:17:48 +0300

    other fields

--- BACKEND/users/serializers.py
+++ BACKEND/users/serializers.py
@@ -6,13 +6,23 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ['id', 'username', 'email']  # Add more fields as needed
 
+
+
 class RegisterUserSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
+    first_name = serializers.CharField(max_length=100)
+    last_name = serializers.CharField(max_length=100)
+    email = serializers.EmailField()
 
     class Meta:
-        model = User
-        fields = ['username', 'password', 'email']  # Fields to include in the serializer
+        model = User  # or your custom user model
+        fields = ('username', 'password', 'email', 'first_name', 'last_name')
 
     def create(self, validated_data):
-        user = User.objects.create_user(**validated_data)
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            password=validated_data['password'],
+            email=validated_data['email'],
+            first_name=validated_data.get('first_name', ''),
+            last_name=validated_data.get('last_name', ''),
+        )
         return user
