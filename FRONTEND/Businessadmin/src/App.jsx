f3b40d4d20b7038ba241ce60c08099171a6ec8de
nzommmo <ericnzomo17@gmail.com>
2024-09-04 12:04:21 +0300

    Waitlist

--- FRONTEND/Businessadmin/src/App.jsx
+++ FRONTEND/Businessadmin/src/App.jsx
@@ -2,15 +2,15 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Homepage from "./components/Homepage/Homepage"
 import Dashboard from './components/Dashboard/Dashboard';
 import Admin from './components/SuperUser/admin';
-
+import Waitlist from './components/Waitlist/Waitlist';
 const App = () => { 
   return (
   <Router>
     <Routes>
       <Route path='/' element={<Homepage/>}/>
       <Route path='/admin' element={<Admin/>}/>
-
       <Route path='/Dashboard' element={<Dashboard/>}/>
+      <Route path='/Waitlist' element={<Waitlist/>}/>
     </Routes>
   </Router>
     
---
+++ FRONTEND/Businessadmin/src/components/Waitlist/Waitlist.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Page from './Page'
+import Header from './Header'
+import Footer from './Footer'
+
+const Waitlist = () => {
+  return (
+    <>
+    <Header/>
+    <Page/>
+    <Footer/>
+
+    </>
+  )
+}
+
+export default Waitlist
